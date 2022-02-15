1a2c1f5f9c2832c5cf9d1d1f9d05252be0582423
sakshishreyaa <sakshishreyaaaaa@gmail.com>
2022-02-15 15:44:35 +0530

    mongo int test

--- src/test_apis.py
+++ src/test_apis.py
@@ -1,3 +1,4 @@
+import asyncio
 from pydoc import cli
 import redis
 import os
@@ -11,4 +12,22 @@ print("testing", redis_db.get('key'))
 
 
 client = AsyncIOMotorClient(str(MONGODB_URL))
-print("mongi", client.config, client.addressable_schedules.meta_data)
+print("mongi", client)
+db = client.addressable_schedules
+
+
+async def do_insert():
+    document = {'key': 'value'}
+    result = await db.meta_data.insert_one(document)
+    print('insert', 'result %s' % repr(result.inserted_id))
+
+
+async def do_find_one():
+    document = await db.meta_data.find_one({'i': {'$lt': 2}})
+    print('find', document)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(do_insert())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(do_find_one())
