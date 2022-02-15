53d18f89c205f761e8dc7806ddd92ab9a225d06a
sakshishreyaa <sakshishreyaaaaa@gmail.com>
2022-02-15 15:23:11 +0530

    mongo int test

--- src/test_apis.py
+++ src/test_apis.py
@@ -4,6 +4,7 @@ import os
 import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 MONGODB_URL = os, os.getenv("MONGODB_URL")
+print("url", MONGODB_URL)
 redis_db = redis.Redis(host="0.0.0.0")
 redis_db.set('key', 'val')
 print("testing", redis_db.get('key'))
